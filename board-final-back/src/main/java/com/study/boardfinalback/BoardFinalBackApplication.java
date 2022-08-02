db468b0ecb2304c1486cd00110fe66b7af5f7c06
mingu <alsrn7590@naver.com>
2022-08-03 00:21:49 +0900

    init

---
+++ board-final-back/src/main/java/com/study/boardfinalback/BoardFinalBackApplication.java
@@ -0,0 +1,13 @@
+package com.study.boardfinalback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardFinalBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardFinalBackApplication.class, args);
+    }
+
+}
