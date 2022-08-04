c0295fc7b54054a14db20ac4b1f5ecb016b1b062
mingu <alsrn7590@naver.com>
2022-08-04 16:49:23 +0900

    feat: BoardRepository, BoardMapper 생성
    BoardRepository.java 생성
    BoardMapper.xml 생성 : BoardRepository 매핑

---
+++ board-final-back/src/main/java/com/study/boardfinalback/repository/BoardRepository.java
@@ -0,0 +1,36 @@
+package com.study.boardfinalback.repository;
+
+import com.study.boardfinalback.domain.Board;
+import com.study.boardfinalback.domain.criteria.SearchCriteria;
+import com.study.boardfinalback.domain.criteria.SearchPagingCriteria;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Mapper
+@Repository
+public interface BoardRepository {
+
+    List<Board> getBoardList();
+
+    List<Board> getBoardListByCategorySeq(int categorySeq);
+
+    int getTotalBoardCountBySearchCriteria(SearchCriteria searchCriteria);
+
+    List<Board> getBoardListBySearchPagingCriteria(SearchPagingCriteria searchPagingCriteria);
+
+    Optional<Board> getBoardBySeq(int boardSeq);
+
+    void addBoard(Board board);
+
+    void updateBoard(Board board);
+
+    void deleteBoard(int boardSeq);
+
+    void updateVisitCount(int boardSeq);
+
+    void updateCommentCount(int boardSeq);
+
+}
