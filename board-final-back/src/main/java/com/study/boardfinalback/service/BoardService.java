002358d3a422a732fa536317a157da2bf28a05d9
mingu <alsrn7590@naver.com>
2022-08-07 22:15:07 +0900

    refactor: BoardService 수정
    boardType에 따른 getTotalBoardCountBySearchCriteria, getBoardListBySearchPagingCriteria 세분화
    주석 처리

--- board-final-back/src/main/java/com/study/boardfinalback/service/BoardService.java
+++ board-final-back/src/main/java/com/study/boardfinalback/service/BoardService.java
@@ -13,8 +13,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
+/**
+ * Board 클래스 관련 비즈니스 로직 구현 클래스
+ */
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -23,22 +25,35 @@ public class BoardService {
     private final BoardRepository boardRepository;
     private final FileRepository fileRepository;
 
+    /**
+     * 모든 Board return
+     *
+     * @return : 모든 Board를 가지는 List
+     */
     public List<Board> getBoardList() {
 
         return boardRepository.getBoardList();
     }
 
-    public List<Board> getBoardListByCategorySeq(int categorySeq) {
+    /**
+     * 검색 조건에 해당하는 Board(boardType: FREE) 총 갯수 return
+     *
+     * @param searchCriteria : 검색 조건
+     * @return : 검색 조건 만족하는 Board(boardType: FREE) 총 갯수
+     */
+    public int getTotalFreeBoardCountBySearchCriteria(SearchCriteria searchCriteria) {
 
-        return boardRepository.getBoardListByCategorySeq(categorySeq);
+        return boardRepository.getTotalFreeBoardCountBySearchCriteria(searchCriteria);
     }
 
-    public int getTotalBoardCountBySearchCriteria(SearchCriteria searchCriteria) {
-
-        return boardRepository.getTotalBoardCountBySearchCriteria(searchCriteria);
-    }
-
-    public List<Board> getBoardListBySearchPagingCriteria(SearchCriteria searchCriteria,
+    /**
+     * 검색 조건과 페이징 조건 만족하는 Board(boardType: FREE) return
+     *
+     * @param searchCriteria : 검색 조건
+     * @param pagingCriteria : 페이징 조건
+     * @return : 검색 조건과 페이징 조건 만족하는 Board(boardType: FREE)들을 가지는 List
+     */
+    public List<Board> getFreeBoardListBySearchPagingCriteria(SearchCriteria searchCriteria,
                                                           PagingCriteria pagingCriteria) {
 
         SearchPagingCriteria searchPagingCriteria = SearchPagingCriteria.builder()
@@ -50,15 +65,127 @@ public class BoardService {
                 .boardCountPerPage(pagingCriteria.getBoardCountPerPage())
                 .build();
 
-        return boardRepository.getBoardListBySearchPagingCriteria(searchPagingCriteria);
+        return boardRepository.getFreeBoardListBySearchPagingCriteria(searchPagingCriteria);
     }
 
+    /**
+     * 검색 조건에 해당하는 Board(boardType: NOTIFY) 총 갯수 return
+     *
+     * @param searchCriteria : 검색 조건
+     * @return : 검색 조건 만족하는 Board(boardType: NOTIFY) 총 갯수
+     */
+    public int getTotalNotifyBoardCountBySearchCriteria(SearchCriteria searchCriteria) {
+
+        return boardRepository.getTotalNotifyBoardCountBySearchCriteria(searchCriteria);
+    }
+
+    /**
+     * 검색 조건과 페이징 조건 만족하는 Board(boardType: NOTIFY) return
+     *
+     * @param searchCriteria : 검색 조건
+     * @param pagingCriteria : 페이징 조건
+     * @return : 검색 조건과 페이징 조건 만족하는 Board(boardType: NOTIFY)들을 가지는 List
+     */
+    public List<Board> getNotifyBoardListBySearchPagingCriteria(SearchCriteria searchCriteria,
+                                                              PagingCriteria pagingCriteria) {
+
+        SearchPagingCriteria searchPagingCriteria = SearchPagingCriteria.builder()
+                .dateCreatedFrom(searchCriteria.getDateCreatedFrom())
+                .dateCreatedTo(searchCriteria.getDateCreatedTo())
+                .categorySeq(searchCriteria.getCategorySeq())
+                .text(searchCriteria.getText())
+                .firstBoardNum(pagingCriteria.getFirstBoardNum())
+                .boardCountPerPage(pagingCriteria.getBoardCountPerPage())
+                .build();
+
+        return boardRepository.getNotifyBoardListBySearchPagingCriteria(searchPagingCriteria);
+    }
+
+    /**
+     * 검색 조건에 해당하는 Board(boardType: MEMBER) 총 갯수 return
+     *
+     * @param searchCriteria : 검색 조건
+     * @return : 검색 조건 만족하는 Board(boardType: MEMBER) 총 갯수
+     */
+    public int getTotalMemberBoardCountBySearchCriteria(SearchCriteria searchCriteria) {
+
+        return boardRepository.getTotalMemberBoardCountBySearchCriteria(searchCriteria);
+    }
+
+    /**
+     * 검색 조건과 페이징 조건 만족하는 Board(boardType: MEMBER) return
+     *
+     * @param searchCriteria : 검색 조건
+     * @param pagingCriteria : 페이징 조건
+     * @return : 검색 조건과 페이징 조건 만족하는 Board(boardType: MEMBER)들을 가지는 List
+     */
+    public List<Board> getMemberBoardListBySearchPagingCriteria(SearchCriteria searchCriteria,
+                                                              PagingCriteria pagingCriteria) {
+
+        SearchPagingCriteria searchPagingCriteria = SearchPagingCriteria.builder()
+                .dateCreatedFrom(searchCriteria.getDateCreatedFrom())
+                .dateCreatedTo(searchCriteria.getDateCreatedTo())
+                .categorySeq(searchCriteria.getCategorySeq())
+                .text(searchCriteria.getText())
+                .firstBoardNum(pagingCriteria.getFirstBoardNum())
+                .boardCountPerPage(pagingCriteria.getBoardCountPerPage())
+                .build();
+
+        return boardRepository.getMemberBoardListBySearchPagingCriteria(searchPagingCriteria);
+    }
+
+    /**
+     * 검색 조건에 해당하는 Board(boardType: NEWS) 총 갯수 return
+     *
+     * @param searchCriteria : 검색 조건
+     * @return : 검색 조건 만족하는 Board(boardType: NEWS) 총 갯수
+     */
+    public int getTotalNewsBoardCountBySearchCriteria(SearchCriteria searchCriteria) {
+
+        return boardRepository.getTotalNewsBoardCountBySearchCriteria(searchCriteria);
+    }
+
+    /**
+     * 검색 조건과 페이징 조건 만족하는 Board(boardType: NEWS) return
+     *
+     * @param searchCriteria : 검색 조건
+     * @param pagingCriteria : 페이징 조건
+     * @return : 검색 조건과 페이징 조건 만족하는 Board(boardType: NEWS)들을 가지는 List
+     */
+    public List<Board> getNewsBoardListBySearchPagingCriteria(SearchCriteria searchCriteria,
+                                                              PagingCriteria pagingCriteria) {
+
+        SearchPagingCriteria searchPagingCriteria = SearchPagingCriteria.builder()
+                .dateCreatedFrom(searchCriteria.getDateCreatedFrom())
+                .dateCreatedTo(searchCriteria.getDateCreatedTo())
+                .categorySeq(searchCriteria.getCategorySeq())
+                .text(searchCriteria.getText())
+                .firstBoardNum(pagingCriteria.getFirstBoardNum())
+                .boardCountPerPage(pagingCriteria.getBoardCountPerPage())
+                .build();
+
+        return boardRepository.getNewsBoardListBySearchPagingCriteria(searchPagingCriteria);
+    }
+
+    /**
+     * 입력받은 boardSeq 가지는 Board return. 해당 Board 없다면 throw BoardNotFoundException
+     *
+     * @param boardSeq : return할 Board의 boardSeq
+     * @return : 입력받은 boardSeq 가지는 Board, 없다면 throw BoardNotFoundException
+     */
     public Board getBoardBySeq(int boardSeq) {
 
         return boardRepository.getBoardBySeq(boardSeq)
                 .orElseThrow(BoardNotFoundException::new);
     }
 
+    /**
+     * 입력받은 Board와 File을 추가한다
+     *
+     * @param board : 추가할 Board의 인스턴스
+     * @param fileList : 추가할 File들의 List
+     * @return : 추가한 Board의 boardSeq
+     */
     @Transactional
     public int addBoard(Board board, List<File> fileList) {
 
@@ -72,6 +199,13 @@ public class BoardService {
         return boardSeq;
     }
 
+    /**
+     * 입력받은 Board를 수정하고, File들을 추가, 삭제한다.
+     *
+     * @param inputBoard : 변경할 필드를 가지는 Board
+     * @param addFileList : 추가할 File List
+     * @param deleteFileList : 삭제할 File List
+     */
     @Transactional
     public void updateBoard(Board inputBoard, List<File> addFileList, List<File> deleteFileList) {
 
@@ -91,15 +225,40 @@ public class BoardService {
         String inputContent = inputBoard.getContent();
         boolean inputFileExist = (fileRepository.getFileListByBoardSeq(boardSeq).size() > 0);
 
-        boardRepository.updateBoard(originBoard.updateBoard(inputTitle, inputContent, inputFileExist));
+        String originTitle = originBoard.getTitle();
+        String originContent = originBoard.getContent();
+        Boolean originFileExist = originBoard.getFileExist();
+
+        Board newBoard = Board.builder().boardSeq(boardSeq).build();
+        if (!originTitle.equals(inputTitle)) {
+            newBoard.setTitle(inputTitle);
+        }
+        if (!originContent.equals(inputContent)) {
+            newBoard.setContent(inputContent);
+        }
+        if (originFileExist != inputFileExist) {
+            newBoard.setFileExist(inputFileExist);
+        }
+
+        boardRepository.updateBoard(newBoard);
     }
 
+    /**
+     * 입력받은 boardSeq 가지는 Board 삭제
+     *
+     * @param boardSeq : 삭제할 Board의 boardSeq
+     */
     @Transactional
     public void deleteBoard(int boardSeq) {
 
         boardRepository.deleteBoard(boardSeq);
     }
 
+    /**
+     * 입력받은 boardSeq 가지는 Board의 visitCount 1 증가
+     * 
+     * @param boardSeq :  update할 Board의 boardSeq
+     */
     @Transactional
     public void updateVisitCount(int boardSeq) {
 
