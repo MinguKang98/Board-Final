26a3327428a8c7105870c721a546996adfc31a34
mingu <alsrn7590@naver.com>
2022-10-09 15:35:22 +0900

    refactor: UserRole.java 구체화

--- board-final-back/src/main/java/com/study/boardfinalback/domain/users/UserRole.java
+++ board-final-back/src/main/java/com/study/boardfinalback/domain/users/UserRole.java
@@ -1,5 +1,33 @@
 package com.study.boardfinalback.domain.users;
 
+
+import java.util.Arrays;
+
+/**
+ * 유저 권한 클래스
+ */
 public enum UserRole {
-    ROLE_MEMBER, ROLE_ADMIN
+
+    ROLE_GUEST("ROLE_GUEST"),
+    ROLE_MEMBER("ROLE_MEMBER"),
+    ROLE_ADMIN("ROLE_ADMIN");
+
+    private String type;
+
+    UserRole(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public static UserRole getUserRole(String type) {
+
+        return Arrays.stream(UserRole.values())
+                .filter(boardType -> type.equals(boardType.getType()))
+                .findAny()
+                .orElse(UserRole.ROLE_GUEST);
+    }
+
 }
